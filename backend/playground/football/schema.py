97491a43723e93445a51e0223bcad2c57b874e11
Ahmed Al-Jawahiry <ahmed@robinai.co.uk>
2020-08-27 16:02:45 +0100

    relay integration and filters

--- backend/playground/football/schema.py
+++ backend/playground/football/schema.py
@@ -1,5 +1,6 @@
-import graphene
+from graphene import relay, ObjectType, Int
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from football.models import Manager, Stadium, Team, Competition, Player
 
 
@@ -7,81 +8,97 @@ class ManagerNode(DjangoObjectType):
     class Meta:
         model = Manager
         fields = ("id", "first_name", "last_name", "age", "team")
+        filter_fields = {
+            "first_name": ["exact", "icontains", "istartswith"],
+            "last_name": ["exact", "icontains", "istartswith"],
+            "age": ["exact", "gte", "lte"],
+        }
+        interfaces = (relay.Node,)
 
 
 class StadiumNode(DjangoObjectType):
     class Meta:
         model = Stadium
         fields = ("id", "name", "city", "capacity")
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+            "city": ["exact", "icontains"],
+            "capacity": ["exact", "gte", "lte"],
+        }
+        interfaces = (relay.Node,)
 
 
 class TeamNode(DjangoObjectType):
     class Meta:
         model = Team
         fields = ("id", "name", "country", "manager", "stadium", "player_set")
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+            "country": ["exact", "icontains"],
+            "manager": ["exact"],
+        }
+        interfaces = (relay.Node,)
 
 
 class CompetitionNode(DjangoObjectType):
     class Meta:
         model = Competition
         fields = ("id", "name", "format", "teams")
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+            "format": ["exact"],
+        }
+        interfaces = (relay.Node,)
 
 
 class PlayerNode(DjangoObjectType):
     class Meta:
         model = Player
         fields = ("id", "first_name", "last_name", "number", "age", "team")
+        filter_fields = {
+            "first_name": ["exact", "icontains", "istartswith"],
+            "last_name": ["exact", "icontains", "istartswith"],
+            "number": ["exact", "gte", "lte"],
+            "age": ["exact", "gte", "lte"],
+            "team": ["exact"],
+        }
+        interfaces = (relay.Node,)
 
 
-def _get_object(model, id: int):
+def _get_object(model, _id: int):
     try:
-        return model.objects.get(id=id)
+        return model.objects.get(id=_id)
     except model.DoesNotExist:
         return None
 
 
-class FootballQuery(graphene.ObjectType):
-    managers = graphene.List(ManagerNode)
-    manager = graphene.Field(ManagerNode, id=graphene.Int(required=True))
+class FootballQuery(ObjectType):
+    managers = DjangoFilterConnectionField(ManagerNode)
+    manager = relay.Node.Field(ManagerNode, id=Int(required=True))
 
-    stadiums = graphene.List(StadiumNode)
-    stadium = graphene.Field(StadiumNode, id=graphene.Int(required=True))
+    stadiums = DjangoFilterConnectionField(StadiumNode)
+    stadium = relay.Node.Field(StadiumNode, id=Int(required=True))
 
-    teams = graphene.List(TeamNode)
-    team = graphene.Field(TeamNode, id=graphene.Int(required=True))
+    teams = DjangoFilterConnectionField(TeamNode)
+    team = relay.Node.Field(TeamNode, id=Int(required=True))
 
-    competitions = graphene.List(CompetitionNode)
-    competition = graphene.Field(CompetitionNode, id=graphene.Int(required=True))
+    competitions = DjangoFilterConnectionField(CompetitionNode)
+    competition = relay.Node.Field(CompetitionNode, id=Int(required=True))
 
-    players = graphene.List(PlayerNode)
-    player = graphene.Field(PlayerNode, id=graphene.Int(required=True))
-
-    def resolve_managers(self, _):
-        return Manager.objects.all()
+    players = DjangoFilterConnectionField(PlayerNode)
+    player = relay.Node.Field(PlayerNode, id=Int(required=True))
 
     def resolve_manager(self, _, id):
         return _get_object(Manager, id)
 
-    def resolve_stadiums(self, _):
-        return Stadium.objects.all()
-
     def resolve_stadium(self, _, id):
         return _get_object(Stadium, id)
-
-    def resolve_teams(self, _):
-        return Team.objects.all().select_related("stadium")
 
     def resolve_team(self, _, id):
         return _get_object(Team, id)
 
-    def resolve_competitions(self, _):
-        return Competition.objects.all().prefetch_related("teams")
-
     def resolve_compeition(self, _, id):
         return _get_object(Competition, id)
-
-    def resolve_players(self, _):
-        return Player.objects.all().select_related("team")
 
     def resolve_player(self, _, id):
         return _get_object(Player, id)
